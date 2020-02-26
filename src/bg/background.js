2d7b9afe954d600f6613ad9c63786a4137f1bc7c
emidio <emidio.torre@gmail.com>
2020-02-26 15:22:20 +0100

    added texts

--- src/bg/background.js
+++ src/bg/background.js
@@ -10,9 +10,9 @@ chrome.storage.local.set({"idx": 0}, function() {
 });
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
+/* chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
-  });
+  }); */
 
